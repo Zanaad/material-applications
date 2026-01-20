e0f8a41e0c01ce161cc1807ebbea897d39689c09
Zanaad <zanaademi7@gmail.com>
2026-01-20 14:13:54 +0100

    docker-compose.yaml for express-app

--- express-app/index.js
+++ express-app/index.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Hello World!</h1> <p>This is an Express.js application running in a Docker container.</p>",
+  );
+});
 
-const PORT = 8080
+const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
